6402c43aac3c95cde0a209a253f96400a361bf29
Araa <79429172+ItzStarlxy@users.noreply.github.com>
2021-12-05 02:23:55 +0800

    Add files via upload

--- src/ItzStarlxy/JoinUI/Main.php
+++ src/ItzStarlxy/JoinUI/Main.php
@@ -1,1 +1,49 @@
+<?php
 
+namespace ItzStarlxy\JoinUI;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+use libs\Vecnavium\FormsUI\SimpleForm;
+use libs\Vecnavium\FormsUI\FormAPI;
+use function str_replace;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable(): void {
+		$this->getLogger()->info("Plugin has Enabled");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveResource("config.yml");
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+	}
+	
+	public function onDisable(): void {
+		$this->getLogger()->info("Plugin has Disable");
+	}
+	
+	public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayerByPrefix();
+		$this->onForm($player);
+	}
+	
+	public function onForm(Player $player){
+		$form = new SimpleForm(function(Player $player, $data = null){
+			if($result === null){
+				return true;
+			}
+			switch($result){
+				case 0:
+				break;
+			}
+		});
+		$form->setTitle(str_replace(["{player}"], [$player->getPlayerByPrefix()], $this->config->get("form-title")));
+		$form->setContent(str_replace(["{player}"], [$player->getPlayerByPrefix()], $this->config->get("form-content")));
+		$form->addButton($this->config->get("close-button"));
+		$form->sendToPlayer($player);
+	}
+}
